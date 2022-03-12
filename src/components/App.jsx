2a7e1b148e6a7a6881308aeb0a54aaee45d24891
Irina1707 <irina_niv@mail.ru>
2022-03-12 23:44:34 +0300

    ДЗ 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,34 @@
+import { Profile } from 'components/Profile/Profile';
+import user from '../user.json';
+import { Statistics } from 'components/Statistics/Statistics';
+import data from '../data.json';
+import { FriendList } from 'components/FriendList/FriendList';
+import friends from '../friends.json';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+import transactions from '../transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}/>
+    
+      <Statistics title="Upload stats" stats={data} />;
+      <Statistics stats={data} />;
+
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
+ </div>
   );
+  
+    
+      
+    
+  
 };
